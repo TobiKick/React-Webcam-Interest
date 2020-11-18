8d3d83da5894a070053d5983028c81d604e3c82d
Tobias Kick <kick.tobias@gmx.de>
2020-11-18 12:40:39 +0800

    added functionality for allowing multiple videos

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -19,9 +19,9 @@ function AppContent () {
     const [isRestarted, setIsRestarted] = useState(false);
     const [capturing, setCapturing] = React.useState(false);
     const playerRef = React.createRef();
-    const [isNextVideo, setIsNextVideo] = React.useState(false);
+    const [currentVideo, setCurrentVideo] = React.useState(0);
 
-    const playList = [{id: "v2kV6pgJxuo", name: "Bosch"}, {id: "K9vFWA1rnWc", name: "Thai"}];
+    const playList = [{id: 0, link: "v2kV6pgJxuo", name: "Bosch"}, {id: 1, link: "K9vFWA1rnWc", name: "Thai"}, {id: 2, link: "yg4Mq5EAEzw", name: "CocaCola"}];
     var val = 0
     const videoConstraints = {
         width: 250,
@@ -70,15 +70,12 @@ function AppContent () {
         document.body.appendChild(a);
         a.style = "display: none";
         a.href = url;
-        
-        if (isNextVideo === false){
-            a.download = playList[0].name + ".webm"
-        } else { a.download = playList[1].name + ".webm" };
+        a.download = playList[currentVideo].name + ".webm"
 
         a.click();
         window.URL.revokeObjectURL(url);
       }
-    }, [recordedChunks, isNextVideo, playList]);
+    }, [recordedChunks, currentVideo, playList]);
 
     const handleInterestSlider = React.useCallback((value) => {
         if(capturing){
@@ -103,30 +100,26 @@ function AppContent () {
     }, [setRecordedChunks, setIsRestarted, setRecordedInterest]);
 
     const handleNextVideo = React.useCallback((value) => {
-        setIsNextVideo(true);
+        setCurrentVideo(currentVideo +1);
         setCapturing(false);
         setIsPaused(true);
         setRecordedChunks([]);
         setRecordedInterest("")
-    }, [setIsNextVideo, setCapturing, setIsPaused, setRecordedChunks, setRecordedInterest]);
+    }, [currentVideo, setCurrentVideo, setCapturing, setIsPaused, setRecordedChunks, setRecordedInterest]);
 
     const handlePreviousVideo = React.useCallback((value) => {
-        setIsNextVideo(false);
+        setCurrentVideo(currentVideo -1);
         setCapturing(false);
         setIsPaused(true);
         setRecordedChunks([]);
         setRecordedInterest("")
-    }, [setIsNextVideo, setCapturing, setIsPaused, setRecordedChunks, setRecordedInterest]);
+    }, [currentVideo, setCurrentVideo, setCapturing, setIsPaused, setRecordedChunks, setRecordedInterest]);
 
     return (
       <React.Fragment>
         <Grid container spacing={3} justify="center" alignItems="flex-start">
           <Grid item xs={9}>
-              {isNextVideo === false ? (
-                    <div style={{pointerEvents: "none"}}><Video ref={playerRef} videoId={playList[0].id} isPaused={isPaused} isRestarted={isRestarted} stopCapturing={handleStopCaptureClick}/></div>
-              ) : (
-                    <div style={{pointerEvents: "none"}}><Video ref={playerRef} videoId={playList[1].id} isPaused={isPaused} isRestarted={isRestarted} stopCapturing={handleStopCaptureClick}/></div>
-              )}
+              <div style={{pointerEvents: "none"}}><Video ref={playerRef} videoId={playList[currentVideo].link} isPaused={isPaused} isRestarted={isRestarted} stopCapturing={handleStopCaptureClick}/></div>
           </Grid>
           <Grid item xs={3}>
               <Grid container justify="center" alignItems="center">
@@ -140,7 +133,7 @@ function AppContent () {
                     ) : recordedChunks.length > 0 ? (
                         <Grid container direction="column" justify="center" alignItems="center" spacing={1}>
                             <Grid item><Button color="primary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleStartCaptureClick}>Resume Capture</Button>
-                            </Grid><Grid item><CSVLink data={recordedInterest} filename={"results_interest.csv"} separator={","}><Button color="primary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleDownload}>Download results</Button></CSVLink>
+                            </Grid><Grid item><CSVLink data={recordedInterest} filename={ playList[currentVideo].name + ".csv"} separator={","}><Button color="primary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleDownload}>Download results</Button></CSVLink>
                             </Grid><Grid item><Button color="secondary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleRestartCaptureClick}>Restart Video</Button>
                             </Grid>
                         </Grid>
@@ -149,16 +142,19 @@ function AppContent () {
                     )}
               </Grid>
            </Grid>
+
            <Grid container justify="space-evenly">
                <Grid item xs={2}>
-                   {isNextVideo === false ? (
-                       <Button color="secondary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleNextVideo}>NEXT VIDEO</Button>
-                    ) : (
-                       <Button color="secondary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handlePreviousVideo}>PREVIOUS VIDEO</Button>
-                    )}
+                   {playList.some(elem => elem.id === (currentVideo -1)) &&
+                        <Button color="secondary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handlePreviousVideo}>PREVIOUS VIDEO</Button>
+                   }
+                   <div><br /></div>
+                   {playList.some(elem => elem.id === (currentVideo +1)) &&
+                        <Button color="secondary" variant="contained" style={{ height: "56px", marginLeft: "20px" }} onClick={handleNextVideo}>NEXT VIDEO</Button>
+                   }
                </Grid>
-               <Grid item xs={3}></Grid>
-               <Grid item xs={5}>
+               <Grid item xs={1}></Grid>
+               <Grid item xs={6}><br />
                <br />
                  <Typography id="discrete-slider-always" gutterBottom>
                    Current level of interest
@@ -188,8 +184,9 @@ function AppContent () {
                           ]}
                    valueLabelDisplay="on"
                  />
+                 <br /><br />
                </Grid>
-               <Grid item xs={2}></Grid>
+               <Grid item xs={3}></Grid>
            </Grid>
         </Grid>
       </React.Fragment>
