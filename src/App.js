d04d4d73584c435bf303c957fb18a795140e30aa
Tobias Kick <kick.tobias@gmx.de>
2020-11-20 23:12:26 +0800

    update with warning box (data)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Paper } from "@material-ui/core";
+import { Paper, Button } from "@material-ui/core";
 import AppContent from "./components/AppContent";
 import Header from "./components/Header";
+import { Alert } from '@material-ui/lab';
 
 const App = () => {
+  const [showAlert, setShowAlert] = React.useState(true);
+
   return (
     <React.Fragment>
       <Header />
+
       <Paper style={{ margin: "20px", padding: "20px", marginTop: "80px" }}>
+        {showAlert ? (
+            <div><Alert variant="outlined" severity="warning"
+              action={
+                <Button onClick={() => setShowAlert(false) } variant="outlined" color="inherit" size="small">
+                  CLOSE
+                </Button>
+              }
+            >
+              A video file of your recording will be created during this experiment --- Please send the output files (video + csv) per e-mail to the researcher. <br />  Received data will be stored for further analysis maximum until the thesis submission!
+            </Alert><br /></div>
+        ) : (
+        <div></div>
+        )}
         <AppContent />
       </Paper>
     </React.Fragment>
